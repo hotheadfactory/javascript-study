b426d59c7a401357c19313e3e8162365b3584623
Jung Hoehyeong <commaniakr@gmail.com>
2019-02-28 19:21:18 +0900

    add: 키보드 입력 받기

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -9,52 +9,69 @@ function operandIn(c) {
 }
 function resetDisplay() {
     document.getElementById('display').value = "";
-    document.getElementById('answer').value = "";
 }
 function typeError() {
     alert("입력 형식이 맞지 않습니다!");
 }
 
 function isValid(display, num) {
-    var a = true;
     if((/[\*\/\+\-]/.exec(display.value)) != null && (num[0] == ""||num[1] == "") || isNaN(num[0])){
-        a = false;
+        return false;
     }
-    return a;
+    return true;
 }
 
+function multiply(splittedNum) {
+    answer = splittedNum[0] * splittedNum[1];
+    return answer;
+}
 
+function divide(splittedNum) {
+    if(splittedNum[1] == '0') {
+        alert("0으로 나눌 수 없음!");
+        answer = 0;
+    }
+    if(splittedNum[1] != '0') {
+        answer = splittedNum[0] / splittedNum[1];
+    }
+    return answer;
+}
+
+function add(splittedNum) {
+    answer = splittedNum[0]*1 + splittedNum[1]*1;
+    return answer;
+}
+
+function subtract(splittedNum) {
+    answer = splittedNum[0] - splittedNum[1];
+    return answer;
+}
 
 function calculate() {
     var display = document.getElementById('display');
-    var num = display.value.split(/[\*\/\+\-]/); // 연산자 기준으로 나눔
+    var splittedNum = display.value.split(/[\*\/\+\-]/); // 연산자 기준으로 나눔
     var answer = ""; // 초기화
   
-    if (isValid(display, num) == false || isNaN(num[0])) {
+    if (isValid(display, splittedNum) == false || isNaN(splittedNum[0])) {
         typeError();
     } // 형식 오류 검출
     if (/\*/.exec(display.value)) {
-        answer = num[0] * num[1];
+        answer = multiply(splittedNum);
     }
     if (/\//.exec(display.value)) {
-        if(num[1] == '0') {
-            alert("0으로 나눌 수 없음!");
-        }
-        if(num[1] != '0') {
-            answer = num[0] / num[1];
-        }
+        answer = divide(splittedNum);
     }
     if (/\+/.exec(display.value)) {
-        answer = num[0]*1 + num[1]*1;
+        answer = add(splittedNum);
     }
     if (/\-/.exec(display.value)) {
-        answer = num[0] - num[1];
+        answer = subtract(splittedNum);
     }
-    if (isValid(display, num)) {
+    if (isValid(display, splittedNum)) {
         document.getElementById('display').value = answer;
     } // Valid한 경우
-    if (!isNaN(num[0]) && isNaN(num[1])) {
-        document.getElementById('display').value = num[0];
+    if (!isNaN(splittedNum[0]) && isNaN(splittedNum[1])) {
+        document.getElementById('display').value = splittedNum[0];
     } // 연산자 없이 숫자만 입력했을 경우
  }
 
---
+++ calculator/keyInput.js
@@ -0,0 +1,64 @@
+window.onkeyup = function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+    var display = document.getElementById('display');
+ 
+    if (key == 49 || key == 97) {
+        console.log("1 pressed!");
+        display.value += '1';
+    }
+    if (key == 50 || key == 98) {
+        console.log("2 pressed!");
+        display.value += '2';
+    }
+    if (key == 51 || key == 99) {
+        console.log("3 pressed!");
+        display.value += '3';
+    }
+    if (key == 52 || key == 100) {
+        console.log("4 pressed!");
+        display.value += '4';
+    }
+    if (key == 53 || key == 101) {
+        console.log("5 pressed!");
+        display.value += '5';
+    }
+    if (key == 54 || key == 102) {
+        console.log("6 pressed!");
+        display.value += '6';
+    }
+    if (key == 55 || key == 103) {
+        console.log("7 pressed!");
+        display.value += '7';
+    }
+    if (key == 56 || key == 104) {
+        console.log("8 pressed!");
+        display.value += '8';
+    }
+    if (key == 57 || key == 105) {
+        console.log("9 pressed!");
+        display.value += '9';
+    }
+    if (key == 106 || key == 42) {
+        console.log("multiply!");
+        calculate();
+        display.value += '*';
+    }
+    if (key == 107 || key == 43) {
+        console.log("add!");
+        calculate();
+        display.value += '+';
+    }
+    if (key == 108 || key == 44) {
+        console.log("subtract!");
+        calculate();
+        display.value += '-';
+    }
+    if (key == 109 || key == 45) {
+        console.log("divide!");
+        calculate();
+        display.value += '/';
+    }
+    if (key == 13) {
+        calculate();
+    }
+ }
