8f6b4564af3986dac3277aa654d5c11a1a26564d
Jung Hoe Hyeong <commaniakr@gmail.com>
2019-02-28 18:25:51 +0900

    fix: 소수점 추가

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,73 +1,55 @@
 function charIn(c) {
-    var input = document.getElementById('input');
-    input.value += c;
+    var display = document.getElementById('display');
+    display.value += c;
 }
-function resetInput() {
-    document.getElementById('input').value = "";
+function resetDisplay() {
+    document.getElementById('display').value = "";
     document.getElementById('answer').value = "";
 }
 function typeError() {
     alert("입력 형식이 맞지 않습니다!");
 }
 
-function isValid(input, num) {
-    if((/[\*\/\+\-]/.exec(input.value)) != null && (num[0] == ""||num[1] == "") || isNaN(num[0])){
-        return false;
+function isValid(display, num) {
+    var a = true;
+    if((/[\*\/\+\-]/.exec(display.value)) != null && (num[0] == ""||num[1] == "") || isNaN(num[0])){
+        a = false;
     }
-    return true;
+    return a;
 }
 
-function multiply(splittedNum) {
-    answer = splittedNum[0] * splittedNum[1];
-    return answer;
-}
 
-function divide(splittedNum) {
-    if(splittedNum[1] == '0') {
-        alert("0으로 나눌 수 없음!");
-        answer = 0;
-    }
-    if(splittedNum[1] != '0') {
-        answer = splittedNum[0] / splittedNum[1];
-    }
-    return answer;
-}
-
-function add(splittedNum) {
-    answer = splittedNum[0]*1 + splittedNum[1]*1;
-    return answer;
-}
-
-function subtract(splittedNum) {
-    answer = splittedNum[0] - splittedNum[1];
-    return answer;
-}
 
 function calculate() {
-    var input = document.getElementById('input');
-    var splittedNum = input.value.split(/[\*\/\+\-]/); // 연산자 기준으로 나눔
+    var display = document.getElementById('display');
+    var num = display.value.split(/[\*\/\+\-]/); // 연산자 기준으로 나눔
     var answer = ""; // 초기화
-    
-    if (isValid(input, splittedNum) == false || isNaN(splittedNum[0])) {
+  
+    if (isValid(display, num) == false || isNaN(num[0])) {
         typeError();
     } // 형식 오류 검출
-    if (/\*/.exec(input.value)) {
-        answer = multiply(splittedNum);
+    if (/\*/.exec(display.value)) {
+        answer = num[0] * num[1];
     }
-    if (/\//.exec(input.value)) {
-        answer = divide(splittedNum);
+    if (/\//.exec(display.value)) {
+        if(num[1] == '0') {
+            alert("0으로 나눌 수 없음!");
+        }
+        if(num[1] != '0') {
+            answer = num[0] / num[1];
+        }
     }
-    if (/\+/.exec(input.value)) {
-        answer = add(splittedNum);
+    if (/\+/.exec(display.value)) {
+        answer = num[0]*1 + num[1]*1;
     }
-    if (/\-/.exec(input.value)) {
-        answer = subtract(splittedNum);
+    if (/\-/.exec(display.value)) {
+        answer = num[0] - num[1];
     }
-    if (isValid(input, splittedNum)) {
-        document.getElementById('answer').value = answer;
+    if (isValid(display, num)) {
+        document.getElementById('display').value = answer;
     } // Valid한 경우
-    if (!isNaN(splittedNum[0]) && isNaN(splittedNum[1])) {
-        document.getElementById('answer').value = splittedNum[0];
+    if (!isNaN(num[0]) && isNaN(num[1])) {
+        document.getElementById('display').value = num[0];
     } // 연산자 없이 숫자만 입력했을 경우
  }
 
