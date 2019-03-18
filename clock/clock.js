eee9933e7d010c092a71db09930f4d53a8ac3629
Jung Hoe Hyeong <commaniakr@gmail.com>
2019-03-18 14:05:27 +0900

    add: 스톱워치 등 전체 기능구현 완료

--- clock/clock.js
+++ clock/clock.js
@@ -1,22 +1,36 @@
-var ddaytimer = setInterval (dayGap, 1);
+const currentTime = setInterval (setClock, 1);
+var isClock = 1;
+var timerStartTime = 0;
+var isStopped = 0;
 
-function dayGap () {
-    var nowday = new Date();
-    nowday = nowday.getTime()+32400000;//밀리세컨드 단위변환, GMT+9
+function setClock() {
+  var nowday = new Date();
+  nowday = nowday.getTime()+32400000;// GMT+9
+  setDisplay(nowday);
+}
+
+function setDisplay(nowday) {
     var d = 0, d1 = 0, d2 = 0, d3 = 0, h = 0, h1 = 0, h2 = 0, m1 = 0, m2 = 0, s1 = 0, s2 = 0, ms1 = 0, ms2 = 0, ms3 = 0;
     var ampm = "AM";
-
     h = Math.floor((nowday / (1000*60*60)) % 24);
-    if(h == 12) {
-      ampm = "PM";
+    if(isClock == 1) {
+      if(h == 12) {
+        ampm = "PM";
+      }
+      if (h == 0) {
+        h = 12;
+        ampm = "AM";
+      }
+      if (h > 12) {
+        h = h-12;
+        ampm = "PM";
+      }
     }
-    if (h == 0) {
-      h = 12;
-      ampm = "AM";
-    }
-    if (h > 12) {
-      h = h-12;
-      ampm = "PM";
+
+    if(isClock == 0) {
+      ampm = "ST";
+      nowday -= timerStartTime;
+      h = Math.floor((nowday / (1000*60*60)) % 24);
     }
 
     h1 = Math.floor(h / 10);//10h
@@ -28,38 +42,32 @@ function dayGap () {
     ms1 = Math.floor((nowday % 1000)/100);//1/10s
     ms2 = Math.floor((nowday % 100)/10);//1/100s
     ms3 = Math.floor(nowday % 10);//milisec
-        document.getElementById("dday-timer").innerHTML = "<br>";
-        document.getElementById("csat2020").style.backgroundImage = "url(sky.jpg)";
-        document.getElementById("time-ampm").innerHTML = ampm+" ";
-        document.getElementById("time-hr1").innerHTML = h1;
-        document.getElementById("time-hr2").innerHTML = h2;
-        document.getElementById("time-min1").innerHTML = m1;
-        document.getElementById("time-min2").innerHTML = m2;
-        document.getElementById("time-sec1").innerHTML = s1;
-        document.getElementById("time-sec2").innerHTML = s2;
-        document.getElementById("time-mili1").innerHTML = ms1;
-        document.getElementById("time-mili2").innerHTML = ms2;
-        document.getElementById("time-mili3").innerHTML = ms3;
+    document.getElementById("dday-timer").innerHTML = "<br>";
+    document.getElementById("time-ampm").innerHTML = ampm+" ";
+    document.getElementById("time-hr1").innerHTML = h1;
+    document.getElementById("time-hr2").innerHTML = h2;
+    document.getElementById("time-min1").innerHTML = m1;
+    document.getElementById("time-min2").innerHTML = m2;
+    document.getElementById("time-sec1").innerHTML = s1;
+    document.getElementById("time-sec2").innerHTML = s2;
+    document.getElementById("time-mili1").innerHTML = ms1;
+    document.getElementById("time-mili2").innerHTML = ms2;
+    document.getElementById("time-mili3").innerHTML = ms3;
 }
 
 function stopWatch() {
-  var nowday = new Date();
+  var time = new Date();
+  time = time.getTime()+32400000;
+  timerStartTime = time;
+  if(isClock == 0) {
+    clearInterval(currentTime);
+    isStopped = 1;
+  }
+  isClock = 0;
 }
 
-function playAudio(audio) {
-  var audio = new Audio();
-  audio.src = './happysky.mp3';
-  audio.currentTime = 0;
-  audio.addEventListener('ended',function(){this.currentTime=0;this.play();},false);
-  var playPromise = audio.play();
-  if (playPromise !== undefined) {
-    playPromise.then(_ => {
-      // Automatic playback started!
-      // Show playing UI.
-    })
-    .catch(error => {
-      // Auto-play was prevented
-      // Show paused UI.
-    });
+function resetStopWatch() {
+  if(isStopped == 1) {
+    window.location.reload(true);
   }
 }
