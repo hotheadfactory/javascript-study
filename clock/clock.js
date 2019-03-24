369cf1b6cf5ec2cfdf5edce70bed70dc73461fdd
Jung Hoe Hyeong <commaniakr@gmail.com>
2019-03-24 16:57:41 +0900

    refactor: 시분초 표시 로직 변경

--- clock/clock.js
+++ clock/clock.js
@@ -1,8 +1,13 @@
 const currentTime = setInterval (setClock, 1);
 
-var isClock = 1;
-var timerStartTime = 0;
-var isStopped = 0;
+let isClock = 1;
+let timerStartTime = 0;
+let isStopped = 0;
+
+function expandDigit(n, width) {
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
 
 function setClock() {
   var nowday = new Date();
@@ -11,7 +16,7 @@ function setClock() {
 }
 
 function setDisplay(nowday) {
-    var d = 0, d1 = 0, d2 = 0, d3 = 0, h = 0, h1 = 0, h2 = 0, m1 = 0, m2 = 0, s1 = 0, s2 = 0, ms1 = 0, ms2 = 0, ms3 = 0;
+    var h = 0, m = 0, s = 0, ms = 0;
     var ampm = "AM";
     h = Math.floor((nowday / (1000*60*60)) % 24);
     if(isClock == 1) {
@@ -33,27 +38,15 @@ function setDisplay(nowday) {
       nowday -= timerStartTime;
       h = Math.floor((nowday / (1000*60*60)) % 24);
     }
-
-    h1 = Math.floor(h / 10);//10h
-    h2 = h % 10;//1h
-    m1 = Math.floor(((nowday / (1000*60)) % 60) / 10);//10m
-    m2 = Math.floor(((nowday / (1000*60)) % 60) % 10);//1m
-    s1 = Math.floor(((nowday / 1000) % 60) / 10);//10s
-    s2 = Math.floor(((nowday / 1000) % 60) % 10);//1s
-    ms1 = Math.floor((nowday % 1000)/100);//1/10s
-    ms2 = Math.floor((nowday % 100)/10);//1/100s
-    ms3 = Math.floor(nowday % 10);//milisec
-    document.getElementById("dday-timer").innerHTML = "<br>";
-    document.getElementById("time-ampm").innerHTML = ampm+" ";
-    document.getElementById("time-hr1").innerHTML = h1;
-    document.getElementById("time-hr2").innerHTML = h2;
-    document.getElementById("time-min1").innerHTML = m1;
-    document.getElementById("time-min2").innerHTML = m2;
-    document.getElementById("time-sec1").innerHTML = s1;
-    document.getElementById("time-sec2").innerHTML = s2;
-    document.getElementById("time-mili1").innerHTML = ms1;
-    document.getElementById("time-mili2").innerHTML = ms2;
-    document.getElementById("time-mili3").innerHTML = ms3;
+    m = Math.floor((nowday / (1000*60)) % 60);//minute
+    s = Math.floor((nowday / 1000) % 60);//second
+    ms = Math.floor(nowday % 1000);// miliseconds
+    document.getElementById("ddayTimer").innerHTML = "<br>";
+    document.getElementById("timeAmpm").innerHTML = ampm+" ";
+    document.getElementById("hour").innerHTML = expandDigit(h, 2);
+    document.getElementById("minute").innerHTML = expandDigit(m, 2);
+    document.getElementById("second").innerHTML = expandDigit(s, 2);
+    document.getElementById("miliseconds").innerHTML = expandDigit(ms, 3);
 }
 
 function stopWatch() {
